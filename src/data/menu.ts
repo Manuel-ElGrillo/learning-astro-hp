062e288195cdc01d9be4a1e6acd8f6306eb84a01
Manuel De Leon <manuel@deleonnet.com>
2023-07-30 00:51:43 -0300

    Project finished

---
+++ src/data/menu.ts
@@ -0,0 +1,16 @@
+import type { MenuItem } from "../interfaces/types";
+
+export const menuItems: MenuItem[] = [
+    {
+        title: "Home",
+        url: "/"
+    },
+    {
+        title: "FAQ",
+        url: "/faq"
+    },
+    {
+        title: "Blog",
+        url: "/blog"
+    },
+]
